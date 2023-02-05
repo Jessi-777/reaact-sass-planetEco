e0fc82ed0e47b0f3dcdf626d4e5415ca79ffb7f8
Jessi Chavez Gallego <angelwerkengineeringmail.com>
2023-02-04 16:53:47 -0800

    renamed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./categories.styles.scss"; 
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Accesories",
+      
+    },
+
+    {
+      id: 3,
+      title: "Shoes",
+      
+    },
+
+    {
+      id: 2,
+      title: "Jackets",
+      
+    },
+   
+    {
+      id: 4,
+      title: "Women",
+      
+    },
+
+    {
+      id: 5,
+      title: "Men",
+      
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="categories-container">
+    {categories.map(({title}) => (
+    <div className="category-container">
+      <div className="background-image" />
+      <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop now</p>
+      </div>
     </div>
+  ))}
+  </div>
   );
 }
 
